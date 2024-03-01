2e870857211d304619cae4306e8d955743fe3277
Sonia <sonya221005@gmail.com>
2024-03-01 11:26:00 +0200

    Added debounce

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,35 @@
 import { MovieList } from '../MovieList';
 import { movies } from '../../utils/movies';
-import { useMemo, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { Movie } from '../../types/Movie';
 import { SearchLine } from '../SearchBar';
 
+function debounce(callback: Function, delay: number) {
+  let timerId = 0;
+
+  return (...args: any) => {
+    window.clearTimeout(timerId);
+
+    timerId = window.setTimeout(() => {
+      callback(...args)
+    }, delay)
+  };
+}
+
 function App() {
   const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+
+  const applyQuery = useCallback(debounce(setAppliedQuery, 700), []);
 
   const visibleMovies: Movie[] = useMemo(() => movies.filter(movie =>
-    movie.title.toLowerCase().includes(query.toLowerCase())
-      || movie.description.toLowerCase().includes(query.toLowerCase())
-  ) as Movie[], [query])
+    movie.title.toLowerCase().includes(appliedQuery.toLowerCase())
+      || movie.description.toLowerCase().includes(appliedQuery.toLowerCase())
+  ) as Movie[], [appliedQuery])
 
   return (
     <div className="App">
-      <SearchLine query={query} setQuery={setQuery} />
+      <SearchLine query={query} setQuery={setQuery} applyQuery={applyQuery} />
       <MovieList movies={visibleMovies} />
     </div>
   );
--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -3,10 +3,11 @@ import './style.scss'
 
 type Props = {
   query: string;
-  setQuery: React.Dispatch<React.SetStateAction<string>>
+  setQuery: React.Dispatch<React.SetStateAction<string>>;
+  applyQuery: (...args: any) => void
 }
 
-export const SearchLine: FC<Props> = ({ query, setQuery }) => (
+export const SearchLine: FC<Props> = ({ query, setQuery, applyQuery }) => (
   <div className="SearchLine">
     <span className="SearchLine__text">Search</span>
 
@@ -14,7 +15,10 @@ export const SearchLine: FC<Props> = ({ query, setQuery }) => (
       className="SearchLine__input"
       type="text"
       value={query}
-      onChange={event => setQuery(event.target.value)}
+      onChange={event => {
+        setQuery(event.target.value);
+        applyQuery(event.target.value);
+      }}
     />
   </div>
 )
